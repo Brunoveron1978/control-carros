13bed019c7f43c4e05a5277212180c23a3c2a5f7
Brunoveron1978 <bruno_veron@hotmail.com>
2025-05-27 09:40:20 -0300

    Add files via upload

---
+++ controlInterno.js
@@ -0,0 +1,106 @@
+export const cajonesCarroParo = [
+  {
+    nombre: "PRIMER CAJON - MEDICACIÓN",
+    color: "bg-red-100",
+    items: [
+      { nombre: "Adrenalina 1mg amp", cantidad: 20 },
+      { nombre: "Atropina 1mg amp", cantidad: 5 },
+      { nombre: "Amiodarona 150mg amp", cantidad: 10 },
+      { nombre: "Digoxina 0,25 mg/ml amp", cantidad: 5 },
+      { nombre: "Noradrenalina 4mg amp", cantidad: 8 },
+      { nombre: "Dopamina 200mg amp", cantidad: 4 },
+      { nombre: "Adenosina 6mg amp", cantidad: 4 },
+      { nombre: "Gluconato de Ca+ amp", cantidad: 5 },
+      { nombre: "Midazolan 15mg Amp", cantidad: 5 },
+      { nombre: "Fenitoina 100mg amp", cantidad: 10 },
+      { nombre: "Dexametasona 8mg amp", cantidad: 4 },
+      { nombre: "Hidrocortisona 500 mg amp", cantidad: 2 },
+      { nombre: "Difenhidramina 10mg amp", cantidad: 10 },
+      { nombre: "Glucosado Hipert 20% amp", cantidad: 5 },
+      { nombre: "Lidocaina 2% 20ml amp", cantidad: 1 },
+      { nombre: "Nitroglicerina 25 mg amp", cantidad: 4 },
+      { nombre: "Isoproterenol 1mg amp", cantidad: 1 },
+      { nombre: "Naloxona 0,4mg amp", cantidad: 4 },
+      { nombre: "Protamina 10mg amp", cantidad: 2 },
+      { nombre: "Cloruro de Potasio 45mEq", cantidad: 4 },
+      { nombre: "Sulfato de Mg+ amp", cantidad: 4 },
+      { nombre: "Salbutamol gotas fco", cantidad: 1 },
+      { nombre: "Ipratropio gotas fco", cantidad: 1 },
+      { nombre: "Isordil comp", cantidad: 2 }
+    ]
+  },
+  {
+    nombre: "SEGUNDO CAJON - DESCARTABLES",
+    color: "bg-yellow-100",
+    items: [
+      { nombre: "Abbocath Nº 14 - 16 c/u", cantidad: 2 },
+      { nombre: "Abbocath Nº 18 - 20 - 22 c/u", cantidad: 3 },
+      { nombre: "Bioconectores", cantidad: 2 },
+      { nombre: "Conectores 4 Puertos", cantidad: 2 },
+      { nombre: "Guias de BIC", cantidad: 3 },
+      { nombre: "Dial-Flow", cantidad: 1 },
+      { nombre: "Guias Macrogotero (V14)", cantidad: 4 },
+      { nombre: "Cinta Adhes 2,5 - 5 cm 1c/u", cantidad: 1 },
+      { nombre: "Aposito Transparente 10x12", cantidad: 2 },
+      { nombre: "Gasa 10 x 10", cantidad: 10 },
+      { nombre: "Jeringas 5-10-20 ml c/u", cantidad: 5 },
+      { nombre: "Agujas 21G 25/8", cantidad: 10 },
+      { nombre: "Agujas 21G 40-50/8 c/u", cantidad: 3 },
+      { nombre: "Agujas 25 G 5/8", cantidad: 3 },
+      { nombre: "Set de Via Central", cantidad: 1 }
+    ]
+  },
+  {
+    nombre: "TERCER CAJON - VENTILACION",
+    color: "bg-green-100",
+    items: [
+      { nombre: "Laringoscopio completo", cantidad: 1 },
+      { nombre: "Pilas Repuesto Laringo", cantidad: 2 },
+      { nombre: "Antiparras", cantidad: 2 },
+      { nombre: "Guantes Esteril 7-71/2-8 c/u", cantidad: 2 },
+      { nombre: "Mandril", cantidad: 3 },
+      { nombre: "Lidocaina jalea", cantidad: 2 },
+      { nombre: "Tubo Endotra. Nº 6-61/2 c/u", cantidad: 2 },
+      { nombre: "Tubo Endotra Nº 7", cantidad: 3 },
+      { nombre: "Tubo Endotra Nº 71/2 - 8 c/u", cantidad: 3 },
+      { nombre: "Tubo endotraqueal Nº 8 1/2", cantidad: 3 },
+      { nombre: "Tubo endotraqueal Nº 9", cantidad: 2 },
+      { nombre: "Másc.Laringea N 4, 5, 6 c/u", cantidad: 1 },
+      { nombre: "Vendas Cambridge 10 cm", cantidad: 2 },
+      { nombre: "Intercambiador de tubo", cantidad: 2 },
+      { nombre: "Medidor de Pres P/Balon (Endotest)", cantidad: 1 },
+      { nombre: "Canulas de Mayo Nº 4-5 c/u", cantidad: 1 },
+      { nombre: "BT 63", cantidad: 2 },
+      { nombre: "Sonda Aspiración (K 32 P)", cantidad: 10 },
+      { nombre: "Pinza Mc Gill – Punta Curva", cantidad: 1 },
+      { nombre: "Mascara NBL", cantidad: 1 },
+      { nombre: "Mascara de Oxigeno", cantidad: 1 },
+      { nombre: "Masc. de Oxi. c/Reservorio", cantidad: 1 }
+    ]
+  },
+   {
+    nombre: "EXTERIOR DEL CARRO",
+    color: "bg-blue-100",
+    items: [
+      { nombre: "Bolsa de reanimacion", cantidad: 1 },
+      { nombre: "Parche Desfib Pads", cantidad: 2 },
+      { nombre: "Tubo de oxigeno", cantidad: 1 },
+      { nombre: "Caja de Guantes desc.", cantidad: 1 },
+      { nombre: "Prolongador de enchufe", cantidad: 1 },
+      { nombre: "Collar Philadelphia (Medio)", cantidad: 1 },
+      { nombre: "Presurizadores", cantidad: 1 }
+    ]
+  }
+];
+
+export const turnosControl = ["TM", "TT", "TN", "Administrativo"];
+
+// Nuevos items para carros específicos (Mantener igual)
+export const itemsCarrosPisoUTH = [
+  { nombre: "Carro de paro cerrado con precinto numerado", cantidad: 1 },
+  { nombre: "3 últimos N° precinto", cantidad: 1 }, // Este es un campo de texto, no Sí/No
+  { nombre: "Bolsa de reanimación", cantidad: 1 },
+  { nombre: "Cardiodesfibrilador probado", cantidad: 1 },
+  { nombre: "Tubo de oxígeno de traslado cargado", cantidad: 1 },
+  { nombre: "Carro de paro limpio, libre de elementos innecesarios", cantidad: 1 }
+];
